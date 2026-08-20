71093183c649713317315eefe150b615ab7bc8ed
Nikita <kizzfromdahood@gmail.com>
2026-08-20 21:10:57 +0300

    Configured the get_wishlist_items() function

--- app/repositories/wishlist_repository.py
+++ app/repositories/wishlist_repository.py
@@ -19,3 +19,42 @@ def add_wishlist_item(wishlist_id, product_id):
 
     finally:
         connection.close()
+
+
+def get_wishlist_items(wishlist_id):
+    connection = get_db_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT
+                    wi.product_id,
+                    p.name,
+                    p.category,
+                    p.price,
+                    p.image
+                FROM wishlist_items wi
+                JOIN products p
+                    ON wi.product_id = p.id
+                WHERE wi.wishlist_id = %s
+                ORDER BY wi.id DESC;
+                """,
+                (wishlist_id,),
+            )
+
+            rows = cursor.fetchall()
+
+            return [
+                {
+                    "product_id": row[0],
+                    "name": row[1],
+                    "category": row[2],
+                    "price": row[3],
+                    "image": row[4],
+                }
+                for row in rows
+            ]
+
+    finally:
+        connection.close()
