4417e61856d2ce92d169af49ce787be2efc1ee2c
Nikita <nikitakiss2003@gmail.com>
2026-08-21 19:01:06 +0300

    Configured variables

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment"
+  type        = string
+}
+
+variable "managed_by" {
+  description = "Tool responsible for managing the infrastructure"
+  type        = string
+  default     = "Terraform"
+}
