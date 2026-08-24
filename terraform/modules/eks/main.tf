7bddc67b434b95e53dc6f973cc6cf351b9eddafc
Nikita <kizzfromdahood@gmail.com>
2026-08-24 14:22:24 +0300

    Configured the EKS Managed Node Group resource

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -19,3 +19,45 @@ resource "aws_eks_cluster" "this" {
   tags = local.common_tags
 }
 
+# EKS Managed Node Group
+
+resource "aws_eks_node_group" "this" {
+  node_group_name = var.eks_node_group_name
+  cluster_name    = var.cluster_name
+  node_role_arn   = aws_iam_role.eks_node_group.arn
+
+  subnet_ids = var.private_subnet_ids
+
+  version = var.cluster_version
+
+  instance_types = var.node_instance_types
+  capacity_type  = var.node_capacity_type
+  disk_size      = var.node_disk_size
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    environment = var.environment
+    node-group  = var.eks_node_group_name
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-${var.eks_node_group_name}"
+    }
+  )
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_ecr_pull_policy,
+  ]
+}
--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -28,6 +28,74 @@ variable "cluster_version" {
   type        = string
 }
 
+# EKS Managed Node Group
+
+variable "eks_node_group_name" {
+  description = "Amazon EKS node group name"
+  type        = string
+}
+
+variable "node_instance_types" {
+  description = "EC2 instance types"
+  type        = list(string)
+}
+
+variable "node_capacity_type" {
+  description = "Capacity type for the EKS managed node group"
+  type        = string
+
+  validation {
+    condition = contains(
+      ["ON_DEMAND", "SPOT"],
+      var.node_capacity_type
+    )
+
+    error_message = "node_capacity_type must be ON_DEMAND or SPOT."
+  }
+}
+
+variable "node_desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+
+  validation {
+    condition     = var.node_desired_size >= 1
+    error_message = "node_desired_size must be at least 1."
+  }
+}
+
+variable "node_min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+
+  validation {
+    condition     = var.node_min_size >= 0
+    error_message = "node_min_size cannot be negative."
+  }
+}
+
+variable "node_max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+
+  validation {
+    condition     = var.node_max_size >= 1
+    error_message = "node_max_size must be at least 1."
+  }
+}
+
+variable "node_disk_size" {
+  description = "Worker node root volume size in GiB"
+  type        = number
+
+  default = 20
+
+  validation {
+    condition     = var.node_disk_size >= 20
+    error_message = "node_disk_size must be at least 20 GB."
+  }
+}
+
 # VPC
 
 variable "vpc_cidr" {
