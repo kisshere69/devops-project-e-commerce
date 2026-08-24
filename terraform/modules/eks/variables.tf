3de8ec6062553e60bf518ee29fbd960f6400c2fd
Nikita <kizzfromdahood@gmail.com>
2026-08-24 14:20:06 +0300

    Configured EKS cluster, default tags, and VPC varirables

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,3 +1,5 @@
+# Default tags
+
 variable "project" {
   description = "Project name"
   type        = string
@@ -12,4 +14,28 @@ variable "managed_by" {
   description = "Tool responsible for managing the infrastructure"
   type        = string
   default     = "Terraform"
+}
+
+# EKS Cluster
+
+variable "cluster_name" {
+  description = "Cluster name"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Amazon EKS cluster version"
+  type        = string
+}
+
+# VPC
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet IDs"
+  type        = list(string)
 }
