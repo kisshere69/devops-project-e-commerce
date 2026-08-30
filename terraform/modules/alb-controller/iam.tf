3a1ce41ba0ab8034d44dbe2398c5decfada9d175
Nikita <kizzfromdahood@gmail.com>
2026-08-30 20:19:34 +0300

    Created a Role for the ALB Controller and attached it

--- terraform/modules/alb-controller/iam.tf
+++ terraform/modules/alb-controller/iam.tf
@@ -1,8 +1,38 @@
 resource "aws_iam_policy" "this" {
-  name = "${var.project}-${var.environment}-alb-controller"
+  name        = "${var.project}-${var.environment}-alb-controller"
   description = "IAM policy for the ALB Controller"
 
   policy = file("${path.module}/iam_policy.json")
 
   tags = local.common_tags
+}
+
+resource "aws_iam_role" "this" {
+  name = "${var.project}-${var.environment}-alb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
 }
