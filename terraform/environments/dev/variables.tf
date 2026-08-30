60e799dddbe38ee0a2eadf1a30e27ecc69a2db36
Nikita <kizzfromdahood@gmail.com>
2026-08-30 09:01:22 +0300

    Specified image tag mutability and scna on push

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -137,3 +137,32 @@ variable "az_b" {
   description = "Availability zone B"
   type        = string
 }
+
+# ECR
+
+variable "repository" {
+  description = "The name of the ECR repository"
+  type        = string
+}
+
+variable "image_tag_mutability" {
+  description = "Image tag mutability settings"
+  type        = string
+
+  default = "MUTABLE"
+
+  validation {
+    condition = contains(
+      ["MUTABLE", "IMMUTABLE"],
+      var.image_tag_mutability
+    )
+
+    error_message = "Image tag mutability must be either 'MUTABLE' or 'IMMUTABLE'."
+  }
+}
+
+variable "scan_on_push" {
+  description = "Enable image scanning on push"
+  type        = bool
+  default     = true
+}
