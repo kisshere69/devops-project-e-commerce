db644dbdce4e874120d5c073573616f3c2466444
Nikita <kizzfromdahood@gmail.com>
2026-08-30 20:20:17 +0300

    Added default variables

---
+++ terraform/modules/alb-controller/variables.tf
@@ -0,0 +1,17 @@
+# Default tags
+
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment"
+  type        = string
+}
+
+variable "managed_by" {
+  description = "Tool responsible for managing the infrastructure"
+  type        = string
+  default     = "Terraform"
+}
