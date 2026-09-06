a9ccebda5ad3a27da8efa6dc1745f9ea7cb13e34
Nikita <kizzfromdahood@gmail.com>
2026-09-06 22:27:24 +0300

    Configured db subnet, security group, and db instance

---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,79 @@
+resource "aws_db_subnet_group" "this" {
+  name = "${var.project}-${var.environment}-db-subnet-group"
+
+  subnet_ids = var.private_subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-db-subnet-group"
+    }
+  )
+}
+
+resource "aws_security_group" "this" {
+  name        = "${var.project}-${var.environment}-rds-sg"
+  description = "Security group for PostgreSQL RDS"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "PostgreSQL access from EKS"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [var.eks_security_group_id]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-rds-sg"
+    }
+  )
+}
+
+resource "aws_db_instance" "this" {
+  identifier = "${var.project}-${var.environment}-postgres"
+
+  engine = "postgres"
+
+  instance_class    = var.instance_class
+  allocated_storage = var.allocated_storage
+  storage_type      = "gp3"
+
+  db_name  = var.db_name
+  username = var.db_username
+  password = var.db_password
+  port     = 5432
+
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [aws_security_group.this.id]
+
+  publicly_accessible = false
+
+  multi_az = false
+
+  storage_encrypted = true
+
+  backup_retention_period = 0
+
+  deletion_protection = false
+  skip_final_snapshot = true
+
+  apply_immediately = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-postgres"
+    }
+  )
+}
