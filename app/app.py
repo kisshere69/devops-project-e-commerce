bfc77586e166a5a23c4ce112d066fec9c09bf2bc
Nikita <kizzfromdahood@gmail.com>
2026-09-09 18:48:55 +0300

    fix: host change

--- app/app.py
+++ app/app.py
@@ -403,7 +403,7 @@ HTTP_REQUEST_DURATION_SECONDS = Histogram(
 
 if __name__ == "__main__":
     app.run(
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=5000,
-        debug=True,
+        debug=False,
     )
