c0f215e3b7fb4a598517b1919f603b65a36eca04
Nikita <kizzfromdahood@gmail.com>
2026-09-09 11:24:11 +0300

    Deleted db_password variable

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -235,12 +235,6 @@ variable "db_username" {
   type        = string
 }
 
-variable "db_password" {
-  description = "Master password for the PostgreSQL database"
-  type        = string
-  sensitive   = true
-}
-
 variable "instance_class" {
   description = "RDS instance class"
   type        = string
