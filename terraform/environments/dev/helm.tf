91dac31537f4eebb49d7d99774122457a7757658
Nikita <kizzfromdahood@gmail.com>
2026-09-09 18:57:46 +0300

    add: helm realeases for secret store csi driver and csi driver aws

---
+++ terraform/environments/dev/helm.tf
@@ -0,0 +1,28 @@
+resource "helm_release" "secrets_store_csi_driver" {
+  name             = "secrets-store-csi-driver"
+  namespace        = "kube-system"
+  create_namespace = false
+
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+}
+
+resource "helm_release" "secrets_store_csi_driver_aws" {
+  name             = "secrets-store-csi-driver-provider-aws"
+  namespace        = "kube-system"
+  create_namespace = false
+
+  repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+  chart      = "secrets-store-csi-driver-provider-aws"
+
+  set = [
+    {
+      name  = "secrets-store-csi-driver.install"
+      value = "false"
+    }
+  ]
+
+  depends_on = [
+    helm_release.secrets_store_csi_driver
+  ]
+}
