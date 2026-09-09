ab21aa3022c774434317b476cf083f751eaa1575
Nikita <kizzfromdahood@gmail.com>
2026-09-09 22:09:31 +0300

    add: set tokenRequests for the helm release

--- terraform/environments/dev/helm.tf
+++ terraform/environments/dev/helm.tf
@@ -5,6 +5,17 @@ resource "helm_release" "secrets_store_csi_driver" {
 
   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
   chart      = "secrets-store-csi-driver"
+
+  set = [
+    {
+      name  = "tokenRequests[0].audience"
+      value = "sts.amazonaws.com"
+    },
+    {
+      name  = "tokenRequests[1].audience"
+      value = "pods.eks.amazonaws.com"
+    }
+  ]
 }
 
 resource "helm_release" "secrets_store_csi_driver_aws" {
