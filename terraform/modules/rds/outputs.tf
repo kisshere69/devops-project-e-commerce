257cc72707561fd0672521bb2df4a2eacdb5252b
Nikita <kizzfromdahood@gmail.com>
2026-09-09 18:55:57 +0300

    add: secret arn and RDS IAM Role Arn for db migration workload

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -2,3 +2,12 @@ output "endpoint" {
   description = "RDS PostgreSQL endpoint"
   value       = aws_db_instance.this.address
 }
+
+output "secret_arn" {
+  value = aws_secretsmanager_secret.rds_credentials.arn
+}
+
+output "db_migration_role_arn" {
+  description = "IAM role ARN used by the database migration workload"
+  value       = aws_iam_role.db_migration.arn
+}
