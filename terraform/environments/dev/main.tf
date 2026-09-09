e41bfe9b42018b1ce74c2921f3f93078a425ed7c
Nikita <kizzfromdahood@gmail.com>
2026-09-09 18:54:30 +0300

    add: helm provider and releases

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -16,6 +16,11 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -25,6 +30,23 @@ provider "aws" {
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name,
+      ]
+    }
+  }
 }
 
 module "vpc" {
