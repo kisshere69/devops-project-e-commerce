450ed4e8d1960e170b7e9c2495610c3bce77ed9e
Nikita <kizzfromdahood@gmail.com>
2026-09-11 17:19:04 +0300

    add: aws_eks_pod_identity_association for app

--- terraform/environments/dev/iam.tf
+++ terraform/environments/dev/iam.tf
@@ -4,3 +4,10 @@ resource "aws_eks_pod_identity_association" "db_migration" {
   service_account = "db-migration"
   role_arn        = module.rds.db_migration_role_arn
 }
+
+resource "aws_eks_pod_identity_association" "app" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "dev"
+  service_account = "roast-co-app"
+  role_arn        = module.rds.app_role_arn
+}
