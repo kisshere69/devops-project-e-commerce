7f3601fb66199e433f619801542a4f4abfbf38ad
Nikita <kizzfromdahood@gmail.com>
2026-09-11 17:18:00 +0300

    add: helm_release for alb controller

--- terraform/environments/dev/helm.tf
+++ terraform/environments/dev/helm.tf
@@ -37,3 +37,39 @@ resource "helm_release" "secrets_store_csi_driver_aws" {
     helm_release.secrets_store_csi_driver
   ]
 }
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name      = "aws-load-balancer-controller"
+  namespace = "kube-system"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  set = [
+    {
+      name  = "clusterName"
+      value = module.eks.cluster_name
+    },
+    {
+      name  = "region"
+      value = var.region
+    },
+    {
+      name  = "vpcId"
+      value = module.vpc.vpc_id
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "alb-controller"
+    }
+  ]
+
+  depends_on = [
+    module.eks,
+    module.alb-controller
+  ]
+}
