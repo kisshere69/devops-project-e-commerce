174660de6413d7c60fc7a68684ebf2fe6c0cf00f
Nikita <kizzfromdahood@gmail.com>
2026-09-11 17:21:06 +0300

    add: app_role_arn output

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -11,3 +11,8 @@ output "db_migration_role_arn" {
   description = "IAM role ARN used by the database migration workload"
   value       = aws_iam_role.db_migration.arn
 }
+
+output "app_role_arn" {
+  description = "IAM role ARN used by the application workload"
+  value       = aws_iam_role.app.arn
+}
